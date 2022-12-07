8bf72704ef73dd7c6c6e70e195b208c4b1678c90
Volkan Yazar <volkanyaza_r@hotmail.com>
2022-12-08 00:45:14 +0300

    Add files via upload

---
+++ VolkanAppEntityFrameWork/VolkanAppEntityFrameWork/Controllers/OgrenciController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace nihat.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        public ViewResult  Index()
+        {
+            ViewData["Okul"] = "Kazan Myo";
+            ViewData["Bolum"] = "Bilgisayar";
+
+            return View();
+        }
+
+        public string OgrenciBilgi(int id)
+        {
+            string ogrenci = "Id gelmedi";
+            if (id == 1)
+            {
+                ogrenci = "Ali veli ";
+            }
+            else if (id == 2)
+            {
+                ogrenci = "osman ";
+            }
+            return ogrenci;
+        }
+    }
+}
